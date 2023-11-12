766040e786e073ae8281d87d9ab026d812b0116a
UBC Student <zivena@students.cs.ubc.ca>
2023-11-12 12:59:49 -0800

    New tailwind-based navbar (#18)
    * basic implementation
    
    * finishing touches on new navbar
    
    * remove redundant old code

--- frontend/src/pages/nav/Nav.js
+++ frontend/src/pages/nav/Nav.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
-import { Outlet } from "react-router-dom";
-import './Nav.css';
+import { Outlet, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import { useState } from 'react';
 
@@ -8,6 +7,7 @@ const Nav = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [cookies, setCookie, removeCookie] = useCookies(['login_cookie']);
   const cookieName = 'login_cookie';
+  const navigate = useNavigate
 
   useEffect(() => {
     if (!!cookies[cookieName]) {
@@ -21,20 +21,67 @@ const Nav = () => {
     removeCookie(['login_cookie']);
   }
 
-  return (
-    <>  
-      <div className="page-layout">
-      <div className="navigation-bar">
-        <a href="/posts">Posts</a>
-        {loggedIn ? <a href={cookies[cookieName]}>Profile</a> : <></>}
-        {!loggedIn ? <a href="/login">Log in</a> : <></>}
-        {!loggedIn ? <a href="/signup">Sign up</a> : <></>}
-        {loggedIn ? <button onClick={onLogOutClick}>Log Out</button> : <></>}
-      </div>
+  const goHome = () => {
+    navigate("/posts");
+  }
 
-      <Outlet />
-      </div>  
+  return (
+    <>
+<nav
+        class="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-500">
+        <div class="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
+            <div class="flex items-center flex-shrink-0 text-gray-800 mr-16">
+                <a href="/posts"
+                   class="block text-xl mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-500 mr-2">
+                    Home
+                </a>
+            </div>
+        </div>
+    
+        <div class="menu w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
+            <div class="text-md font-bold text-blue-500 lg:flex-grow">
+                {/* THIS IS WHERE WE WOULD PUT LINKS TO PAGES IF WE WANTED TO */}
+                {/* <a href="/posts"
+                   class="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-500 mr-2">
+                    Feed
+                </a> */}
+            </div>
+            <div class="relative mx-auto text-gray-600 lg:block hidden">
+
+                <input
+                    class="border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none"
+                    type="search" name="search" placeholder="Search"/>
+                <button type="submit" class="absolute right-0 top-0 mt-3 mr-2">
+                    <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                         version="1.1" id="Capa_1" x="0px" y="0px"
+                         viewBox="0 0 56.966 56.966" 
+
+                         width="512px" height="512px">
+                <path
+                    d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
+              </svg>
+                </button>
+            </div>
+            <div class="flex ">
+                {!loggedIn ? <a href="/login"
+                   class="block text-md px-4 py-2 rounded text-blue-500 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-500 lg:mt-0">Sign
+                    in</a> : <></>}
+                    {!loggedIn ? <a href="/signup"
+                   class="block text-md px-4 py-2 rounded text-blue-500 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-500 lg:mt-0">Sign
+                    up</a> : <></>}
+                    {loggedIn ? <a href={cookies[cookieName]}
+                   class="block text-md px-4 py-2 rounded text-blue-500 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-500 lg:mt-0 cursor:pointer">Profile</a> : <></>}  
+                    {loggedIn ? <a onClick={onLogOutClick}
+                   class="block text-md px-4 py-2 rounded text-blue-500 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-500 lg:mt-0 cursor:pointer">Log
+                    out</a> : <></>}    
+    
+                </div>
+        </div>
+    
+    </nav>
+    <Outlet />
     </>
+    
   )
 };
 
