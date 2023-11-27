a2ce62f7d1d096b48db248ce5f59afdb727abbcf
Youssef Saleh <ymseam@hotmail.com>
2023-11-26 21:35:11 -0800

    fixed Nav

--- frontend/src/pages/nav/Nav.js
+++ frontend/src/pages/nav/Nav.js
@@ -39,37 +39,28 @@ const Nav = () => {
   // changes to the template to style it with our project's specific goals.
   return (
     <>
-      <nav class="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-500">
-        <div class="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
-          <div class="flex items-center flex-shrink-0 text-gray-800 mr-16">
+      <nav className="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-500">
+        <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
+          <div className="flex items-center flex-shrink-0 text-gray-800 mr-16">
             <a
               href="/posts"
-              class="block text-xl mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-500 mr-2"
+              className="block text-xl mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-500 mr-2"
             >
               Home
             </a>
             <a
               href="/visualizer"
-              class="block text-l mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-500 mr-2"
+              className="block text-xl mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-500 mr-2"
             >
               Visualizer
             </a>
             <a
               href="/stats"
-              class="block text-lg mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-500 mr-2"
+              className="block text-xl mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-500 mr-2"
             >
               Stats
             </a>
           </div>
-<nav
-        className="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-500">
-        <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
-            <div className="flex items-center flex-shrink-0 text-gray-800 mr-16">
-                <a href="/posts"
-                   className="block text-xl mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-500 mr-2">
-                    Home
-                </a>
-            </div>
         </div>
 
         <div className="menu w-full flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
@@ -95,11 +86,15 @@ const Nav = () => {
               className="absolute right-0 top-0 mt-3 mr-2"
             >
               <svg
-                className="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
-                         version="1.1" id="Capa_1" x="0px" y="0px"
-                         viewBox="0 0 56.966 56.966"
-
-                         width="512px" height="512px"
+                className="text-gray-600 h-4 w-4 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                id="Capa_1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 56.966 56.966"
+                width="512px"
+                height="512px"
               >
                 <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
               </svg>
@@ -145,14 +140,12 @@ const Nav = () => {
               </a>
             ) : (
               <></>
-)}
+            )}
           </div>
         </div>
-
-    </nav>
-    <Outlet />
+      </nav>
+      <Outlet />
     </>
-
   );
 };
 
