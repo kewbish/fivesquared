1a85af5213f677a705e70b4ae33879214c653637
Emilie Ma <kewbish@students.cs.ubc.ca>
2023-11-30 10:27:35 -0800

    Add trigger for badges

--- frontend/src/components/badgeCard/BadgeCard.js
+++ frontend/src/components/badgeCard/BadgeCard.js
@@ -5,7 +5,7 @@ function BadgeCard({ badgeData }) {
 	// changes to the template to style it with our project's specific goals.
 	return (
 		<div className="flex flex-col justify-center max-w-xs p-6 shadow-md rounded-xl sm:px-12 bg-gray-50 text-gray-900">
-			<img src={badgeData.icon_url} alt="" className="w-32 h-32 mx-auto rounded-full bg-gray-500 aspect-square" />
+			<img src={badgeData.icon_url} alt="" className="w-32 h-32 mx-auto rounded-full bg-gray-500 object-cover" />
 			<div className="space-y-4 text-center">
 				<div className="my-2 space-y-1">
 					<h2 className="text-xl font-semibold sm:text-2xl">{badgeData.name}</h2>
